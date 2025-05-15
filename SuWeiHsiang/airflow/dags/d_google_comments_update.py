7f43e34c04c49d265c5baac8affd64dd8b044b0d
LAPTOP-E6T9QUMT\Tibame <scottjasontaiwan@yahoo.com.tw>
2025-05-15 14:40:02 +0800

    加入task group

--- SuWeiHsiang/airflow/dags/d_google_comments_update.py
+++ SuWeiHsiang/airflow/dags/d_google_comments_update.py
@@ -1,11 +1,12 @@
 from airflow.decorators import dag,task
-from tasks.collect_all import all_restaurants_comments
+from tasks.collect_all import get_restaurants_info,get_batch_store,create_group,all_restaurants_comments
 
 from datetime import datetime,timedelta
 
+import pandas as pd
+
 default_args = {
     "owner":"airflow",
-    "depend_on_past":False,
     "email":["your_email@example.com"],
     "email_on_failure":False,
     "email_on_retry":False,
@@ -17,13 +18,19 @@ default_args = {
     dag_id="d_google_comments_update",
     default_args=default_args,
     description="get new google map restaurants comment daily",
+    max_active_tasks=10,
     schedule_interval="0 1 * * *",
     start_date=datetime(2025,5,3),
-    catchup=False
+    catchup=False,
+    tags=["update comments from all restaurant"]
 )
 
 def d_google_comments_update():
-
-    all_restaurants_comments(0,"")
-
+    
+    restaurants = get_restaurants_info()
+    batch1 = get_batch_store.override(task_id="get_batch_0")(restaurants,0,1000)
+    batch2 = get_batch_store.override(task_id="get_batch_1")(restaurants,1,1000)
+    create_group(1,batch1)
+    create_group(2,batch2)
+        
 d_google_comments_update()
--- SuWeiHsiang/airflow/tasks/collect_all.py
+++ SuWeiHsiang/airflow/tasks/collect_all.py
@@ -5,18 +5,29 @@ from utils.collect_one import one_restaurant_comments
 from utils.save_error import save_error_info
 
 from airflow.decorators import task
+from airflow.utils.task_group import TaskGroup
 
 @task
-def all_restaurants_comments(start,end):
-    df = pd.read_csv(f"./data/restaurants.csv")
-    count = 1
-    if end == "" : end = df.shape[0]
-    for name,link in zip(df.iloc[start:end,0],df.iloc[start:end,2]) :
-        print(f"爬取第{start + count}家評價，尚餘{end - start - count}家待爬取")
-        try :
-            one_restaurant_comments(name,link)
-        except Exception as e:
-            save_error_info(name,start,count,e)
-        
-        time.sleep(1)
-        count += 1
+def get_restaurants_info():
+    df = pd.read_csv(f"./data/restaurants.csv",encoding="utf-8-sig")[["st_name", "st_url"]]
+    return df.to_dict(orient="records")
+
+@task
+def get_batch_store(stores,batch_num,batch_size):
+    start = batch_size * batch_num
+    end   = start + batch_size
+    return stores[start:end]
+
+def create_group(batch_num,store):
+    with TaskGroup(group_id=f"collect_comments_group_{batch_num}") as group:
+        all_restaurants_comments.expand(store=store)
+
+@task
+def all_restaurants_comments(store):
+    name = store["st_name"]
+    link = store["st_url"]
+    print(f"正在爬取{name}的評論")
+    try :
+        one_restaurant_comments(name,link)
+    except Exception as e:
+        save_error_info(name,e)
